703a19b68cba7125d8f238f7ccb862542b2b07e8
kijun <kijunseo@gmail.com>
2015-01-25 13:42:13 -0800

    airport scene

---
+++ GGJ15/Assets/AirportSceneScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AirportSceneScript : MonoBehaviour {
+
+	public Text textUI;
+	public Image imageUI;
+	public Sprite byeImage;
+	public FadeOut fader;
+	public AudioSource airportSource;
+	public AudioClip airportSound;
+	public AudioClip takeoffSound;
+
+	bool fadeAudio = false;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("ShowText", 2);
+		Invoke ("ShowLeaving", 5);
+		Invoke ("FadeOut", 10);
+		Invoke ("NextScene", 20);
+	}
+
+	void ShowText() {
+		airportSource.clip = airportSound;
+		airportSource.Play ();
+		textUI.enabled = true;
+	}
+
+	void ShowLeaving() {
+		textUI.enabled = false;
+		imageUI.sprite = byeImage;
+		fadeAudio = true;
+	}
+	
+	void FadeOut() {
+		fader.Out ();
+		airportSource.clip = takeoffSound;
+		airportSource.volume = 1;
+		airportSource.Play ();
+	}
+
+	void NextScene() {
+		Application.LoadLevel("call1");
+	}
+
+	void Update() {
+		if (fadeAudio) {
+			airportSource.volume = Mathf.Lerp (airportSource.volume, 0, Time.deltaTime * .1f);
+		}
+	}
+}
---
+++ GGJ15/Assets/FadeOut.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+	private RawImage box;
+	private bool startFade;
+	// Use this for initialization
+	void Start (){
+		box = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startFade) {
+			float fader = Mathf.Lerp (box.color.a, 255, Time.deltaTime * .0008f);
+			box.color = new Color (0f, 0f, 0f, fader);
+		}
+	}
+
+	public void Out() {
+		startFade = true;
+	}
+}
