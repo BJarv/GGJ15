9c43cc9c3945867e082904e06f08dedff7ad4d0f
kijun <kijunseo@gmail.com>
2015-01-25 12:25:09 -0800

    add gun scene

--- GGJ15/Assets/SceneNavigator.cs
+++ GGJ15/Assets/SceneNavigator.cs
@@ -8,7 +8,7 @@ public class SceneNavigator : MonoBehaviour {
 	public float timeout;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		if (useTimer) {
 			Invoke("PlayNextScene", timeout);
 		}
