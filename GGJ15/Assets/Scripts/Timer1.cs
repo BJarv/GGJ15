ae680999cebff9968fa01a4c7bdfc7fc1d20ada3
TVTjannel <daniel.tjandra96@gmail.com>
2015-01-24 16:19:01 -0800

    Added 1st Phone Sequence

---
+++ GGJ15/Assets/Scripts/Timer1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer1 : MonoBehaviour {
+	private float tm= 0.0f;
+	public Text count;
+	float minutes;
+	int tmin;
+	float seconds;
+	int tsec;
+	// Update is called once per frame
+	void FixedUpdate() {
+		tm += Time.deltaTime;
+		string minutes = Mathf.Floor (tm / 60).ToString ("00");
+		string seconds = (tm % 60).ToString ("00");
+		count.text =  minutes.ToString () + " : "+seconds.ToString ();
+	}
+}
