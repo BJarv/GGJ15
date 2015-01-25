733d7be24b0d924d6c7d01c5caf8a766a1a113ba
kijun <kijunseo@gmail.com>
2015-01-24 18:39:42 -0800

    better fish game

--- GGJ15/Assets/Scripts/Fish/PumpController.cs
+++ GGJ15/Assets/Scripts/Fish/PumpController.cs
@@ -8,7 +8,6 @@ public class PumpController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		bubbles.renderer.sortingLayerName = "Effects";
-		Debug.Log (bubbles.renderer.sortingLayerName);
 	}
 	
 	// Update is called once per frame
